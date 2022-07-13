e7ba421ab1575f3b010092ed91423feca8e3d160
Oespin1226 <oespinalcastillo@horizon.csueastbay.edu>
2022-07-13 13:07:02 -0700

    added files

---
+++ FG Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FG Tracker
+//
+//  Created by Osiel Espinal Castillo on 7/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-FG Tracker/Pods-FG Tracker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FG_Tracker : NSObject
+@end
+@implementation PodsDummy_Pods_FG_Tracker
+@end
